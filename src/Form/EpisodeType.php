d86c90ed37b3dfca50d0cedb26ed122179407a16
Erika <erika.ike@outlook.fr>
2023-06-12 12:01:43 +0200

    fixed last issues

--- src/Form/EpisodeType.php
+++ src/Form/EpisodeType.php
@@ -15,7 +15,7 @@ class EpisodeType extends AbstractType
             ->add('title')
             ->add('number')
             ->add('synopsis')
-            ->add('season_id', null, ['choice_label' => 'number']);
+            ->add('season', null, ['choice_label' => 'number']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
